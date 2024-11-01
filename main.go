213e66627e715a89e8deaefdf8a3eea3d3bee1dd
rosen1000 <rosen.ruseff@gmail.com>
2024-11-01 13:08:32 +0200

    added verbose and output arguments

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"sync"
 	"time"
@@ -14,15 +15,20 @@ import (
 )
 
 var cleanup = flag.Bool("cleanup", false, "Clean up converted files")
+var output = flag.String("out", "", "Path for converted files to be placed")
+var verbose = flag.Bool("verbose", false, "Enable verbose logging")
 var wg = new(sync.WaitGroup)
 
 func init() {
 	getopt.Alias("c", "cleanup")
+	getopt.Alias("o", "out")
+	getopt.Alias("v", "verbose")
 	flag.Usage = func() {
 		fmt.Printf("Usage:\n%v <options...> [format] [files...]\n\nOptions:\n", os.Args[0])
 		getopt.PrintDefaults()
 	}
 	getopt.Parse()
+	vlog("Verbose mode enabled")
 }
 
 func main() {
@@ -40,12 +46,19 @@ func main() {
 		go convertFile(file, format)
 	}
 	wg.Wait()
-	fmt.Printf("Done! (%vms)\n", time.Now().Sub(now).Milliseconds())
+	fmt.Printf("Done! (%vms)\n", time.Since(now).Milliseconds())
 }
 
 func convertFile(fileName string, format string) {
+	vlog("Converting " + fileName + "...")
 	defer wg.Done()
-	cmd := exec.Command("ffmpeg", "-hide_banner", "-i", fileName, formatName(fileName, format))
+	if *output != "" {
+		if _, err := os.Stat(*output); err != nil {
+			vlog("Making path: " + *output)
+			os.MkdirAll(*output, 0755)
+		}
+	}
+	cmd := exec.Command("ffmpeg", "-hide_banner", "-i", fileName, path.Join(*output, formatName(fileName, format)))
 	errb := new(bytes.Buffer)
 	cmd.Stderr = errb
 	err := cmd.Run()
@@ -62,15 +75,25 @@ func convertFile(fileName string, format string) {
 	}
 
 	if *cleanup {
+		vlog("Deleting" + fileName + "...")
 		err := os.Remove(fileName)
 		if err != nil {
 			fmt.Printf("Unable to remove %v (%v)\n", fileName, err.Error())
 		}
+		vlog("Done deleting " + fileName)
 	}
+
+	vlog("Done ceonverting " + fileName + " -> " + formatName(fileName, format))
 }
 
 func formatName(fileName string, format string) string {
 	split := strings.Split(fileName, ".")
 	name := strings.Join(split[:len(split)-1], ".")
 	return (name + "." + format)
+}
+
+func vlog(text string) {
+	if *verbose {
+		println(text)
+	}
 }
